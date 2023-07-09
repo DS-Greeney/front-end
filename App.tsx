3ac7e135050dbee821b3c738d8b59d419f38dfd8
박유정 <p_1321@duksung.ac.kr>
2023-07-09 20:59:21 +0900

    tabBarIcon

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Home from './src/Home';
 import Search from './src/Search';
@@ -13,17 +14,45 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={Home} options={{title: '홈'}} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: '홈',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            ),
+          }}
+        />
         <Stack.Screen
           name="Search"
           component={Search}
-          options={{title: '검색'}}
+          options={{
+            title: '검색',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="search" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen name="Map" component={Map} options={{title: '지도'}} />
+        <Stack.Screen
+          name="Map"
+          component={Map}
+          options={{
+            title: '지도',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="map" color={color} size={size} />
+            ),
+          }}
+        />
         <Stack.Screen
           name="Mypage"
           component={Mypage}
-          options={{title: '마이'}}
+          options={{
+            title: '마이',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="search" color={color} size={size} />
+            ),
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
