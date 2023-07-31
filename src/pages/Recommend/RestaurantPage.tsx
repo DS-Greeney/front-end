852b05e685157ac2679ca00c117afa602b5ede5b
won0324 <huiwon0324@naver.com>
2023-07-31 18:51:15 +0900

    feat: RestaurantPage, Veganspot

--- src/pages/Recommend/RestaurantPage.tsx
+++ src/pages/Recommend/RestaurantPage.tsx
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, ScrollView, FlatList} from 'react-native';
 import Header from '../../components/Common/Header';
 import SearchBar from '../../components/SearchBar';
 import FilterList from '../../components/filter/FilterList';
+import Veganspot from '../../components/Recommend/Veganspot';
 import {useNavigation} from '@react-navigation/native';
 
 const areaList = [
@@ -26,6 +27,57 @@ const areaList = [
   '제주',
 ];
 
+const dummyVeganData = [
+  {
+    image:
+      'https://d12zq4w4guyljn.cloudfront.net/750_750_20220808095252388_photo_4f5c74b01ea8.jpg',
+    type: '양식',
+    name: '공간녹음',
+    location: '서울 강서구 마곡동',
+    scope: 5,
+  },
+  {
+    image:
+      'https://media-cdn.tripadvisor.com/media/photo-p/1b/3d/74/6d/getlstd-property-photo.jpg',
+    type: '이탈리아 음식',
+    name: '마히나 비건 테이블',
+    location: '서울 강서구 신사동',
+    scope: 4.3,
+  },
+  {
+    image:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMTA5MThfMTc1/MDAxNjMxOTQxOTgyOTUy._Cxioy8tfC-V0TxBsqD3hpfU0zBep10rUATABrgXlA8g.HtLvm3ZcAwLKlMPaGfuvTBAX5qUYhFG9PinlmrZFQSAg.JPEG.dbdnjswls590/IMG_0579.JPG?type=w800',
+    type: '양식',
+    name: '비건 앤 비욘드',
+    location: '서울 서대문구 연희동',
+    scope: 4.2,
+  },
+  {
+    image:
+      'https://d12zq4w4guyljn.cloudfront.net/750_750_20220808095252388_photo_4f5c74b01ea8.jpg',
+    type: '양식',
+    name: '셰발레리',
+    location: '서울 마포구 합정동',
+    scope: 4.0,
+  },
+  {
+    image:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMzAzMTRfNTQg/MDAxNjc4NzY1ODQwNjY4.CZkmclYBg_G_rMuMc81ZdWeXPPJmPW2t3a6YAnD5Yh0g.xipjo8NNN6Ga-0bBp5eoboogZCuvyCwIw4b4rFkjQukg.JPEG.sarang_11/IMG_5185.JPG?type=w800',
+    type: '양식',
+    name: '루티드',
+    location: '서울 송파구 방희동',
+    scope: 3.8,
+  },
+  {
+    image:
+      'https://d12zq4w4guyljn.cloudfront.net/750_750_20220808095252388_photo_4f5c74b01ea8.jpg',
+    type: '퓨전음식',
+    name: '드렁큰비건',
+    location: '서울 마포구 창전동',
+    scope: 3.5,
+  },
+];
+
 export default function TourspotPage() {
   let navigation = useNavigation();
 
@@ -37,6 +89,13 @@ export default function TourspotPage() {
 
       <View style={styles.container}>
         <View style={styles.line} />
+        <FlatList
+          style={styles.veganspots}
+          data={dummyVeganData}
+          renderItem={({item}) => (
+            <Veganspot data={item} navigation={navigation} />
+          )}
+        />
       </View>
     </View>
   );
@@ -55,5 +114,8 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderWidth: 0.5,
     borderColor: '#B1B1B1',
+  },
+  veganspots: {
+    flex: 1,
   },
 });
