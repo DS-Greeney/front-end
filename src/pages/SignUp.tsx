d81f23feb9dafacf503a4339fea5b19e9b1f6167
박유정 <p_1321@duksung.ac.kr>
2023-09-16 02:22:14 +0900

    회원가입 css 수정 및 비밀번호 확인 기능 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -8,89 +8,24 @@ import {
   Alert,
 } from 'react-native';
 import {useState} from 'react';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import axios from 'axios';
 
 import Header from '../components/Common/Header';
-import FilterBtnGender from '../components/filter/FilterBtnGender';
 
 export default function SignUp({navigation}: any) {
-  Date.prototype.format = function (f) {
-    if (!this.valueOf()) return ' ';
-    var d = this;
-    return f.replace(/(yyyy|MM|dd)/gi, function ($1) {
-      switch ($1) {
-        case "yyyy": return d.getFullYear();
-        case "MM": return (d.getMonth() + 1).zf(2);
-        case "dd": return d.getDate().zf(2);
-        default: return $1;
-      }
-    });
-  };
-
-  String.prototype.string = function (len) { var s = '', i = 0; while (i++ < len) { s += this; } return s; };
-  String.prototype.zf = function (len) { return "0".string(len - this.length) + this; };
-  Number.prototype.zf = function (len) { return this.toString().zf(len); };
-
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const placeholder = '날짜를 입력해주세요';
-  const [text, onChangeText] = useState('');
-
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date) => {
-    hideDatePicker();
-    onChangeText(date.format('yyyy년 MM월 dd일'));
-    handleInputChange('userBirthdate', date.format('yyyyMMdd'));
-  };
-
-  const [selectArea, setSelectArea] = useState('전체');
-
-  const handleFilterClick = (name: string) => {
-    setSelectArea(name);
-    handleInputChange('userGender', name);
-  };
-
-  const dummySpotData = [
-    {
-      userNickname: '강수민',
-      userEmail: 'ski03257@naver.com',
-      userPassword: '1234',
-      userPhonenum: '01033972435',
-      userBirthdate: '20001030',
-      userGender: 2,
-    },
-  ];
-
   interface UserModel {
     userNickname: string;
     userEmail: string;
     userPassword: string;
     userPasswordchk: string;
-    userPhonenum: string;
-    userBirthdate: string;
-    userGender: string;
-    userGendernum: number;
   }
   const [user, setUser] = useState<UserModel>({
     userNickname: '',
     userEmail: '',
     userPassword: '',
     userPasswordchk: '',
-    userPhonenum: '',
-    userBirthdate: '',
-    userGender: '',
-    userGendernum: 0,
   });
-
-  const genderList = ['남자', '여자', '선택 안 함'];
 
   const handleInputChange = (key: string, value: string) => {
     setUser(prevState => ({
@@ -99,18 +34,18 @@ export default function SignUp({navigation}: any) {
     }));
   };
 
-  function genderChange() {
-    if (user.userGender === '남자') {
-      user.userGendernum = 1;
-    } else if (user.userGender === '여자') {
-      user.userGendernum = 2;
-    } else if (user.userGender === '선택 안 함') {
-      user.userGendernum = 3;
+  const [passchk, setPasschk] = useState('');
+  const handleCheck = (key: string, value: string) => {
+    handleInputChange(key, value);
+    console.log(user);
+    if (user.userPassword === value || user.userPasswordchk === value) {
+      setPasschk('비밀번호가 일치합니다.');
+    } else {
+      setPasschk('비밀번호가 다릅니다. 다시 입력해주세요.');
     }
-  }
+  };
 
   function handleClick() {
-    genderChange();
     console.log(user);
     if (user.userPassword === user.userPasswordchk) {
       axios
@@ -118,9 +53,6 @@ export default function SignUp({navigation}: any) {
           userNickname: user.userNickname,
           userEmail: user.userEmail,
           userPassword: user.userPassword,
-          userPhonenum: user.userPhonenum,
-          userBirthdate: user.userBirthdate,
-          userGender: user.userGendernum,
         })
         .then(function (response) {
           console.log(response.data);
@@ -182,7 +114,7 @@ export default function SignUp({navigation}: any) {
             style={styles.textInput}
             secureTextEntry={true}
             onChangeText={text => {
-              handleInputChange('userPassword', text);
+              handleCheck('userPassword', text);
             }}
           />
         </View>
@@ -194,79 +126,11 @@ export default function SignUp({navigation}: any) {
             style={styles.textInput}
             secureTextEntry={true}
             onChangeText={text => {
-              handleInputChange('userPasswordchk', text);
+              handleCheck('userPasswordchk', text);
             }}
           />
         </View>
-        <Text style={[styles.smallText, {marginLeft: 120, marginTop: 5}]}>비밀번호가 일치합니다.</Text>
-        <View style={styles.textView}>
-          <TouchableOpacity disabled={true} style={styles.textBox}>
-            <Text style={styles.text}>휴대폰 번호</Text>
-          </TouchableOpacity>
-          <TextInput
-            keyboardType="phone-pad"
-            maxLength={11}
-            style={[styles.numInput, {width: 183}]}
-            onChangeText={text => {
-              handleInputChange('userPhonenum', text);
-            }}
-          />
-          <TouchableOpacity style={styles.chkbutton}>
-            <Text style={styles.smallText}>인증하기</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={[styles.smallText, {marginLeft: 120, marginTop: 5}]}>인증 완료되었습니다.</Text>
-        <View style={styles.textView}>
-          <TouchableOpacity disabled={true} style={styles.textBox}>
-            <Text style={styles.text}>생년월일</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={showDatePicker}>
-            <TextInput
-              pointerEvents="none"
-              style={styles.dateInput}
-              placeholder={placeholder}
-              placeholderTextColor="#333"
-              underlineColorAndroid="transparent"
-              editable={false}
-              value={text}
-              onChangeText={text => {
-                handleInputChange('userBirthdate', text);
-              }}
-            />
-            <DateTimePickerModal
-              isVisible={isDatePickerVisible}
-              mode="date"
-              onConfirm={handleConfirm}
-              onCancel={hideDatePicker}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.textView}>
-          <TouchableOpacity disabled={true} style={styles.textBox}>
-            <Text style={styles.text}>성별</Text>
-          </TouchableOpacity>
-          {genderList.map((area, idx) => {
-            if (selectArea.includes(area)) {
-              return (
-                <FilterBtnGender
-                  key={idx}
-                  name={area}
-                  selected={true}
-                  onPress={handleFilterClick}
-                />
-              );
-            } else {
-              return (
-                <FilterBtnGender
-                  key={idx}
-                  name={area}
-                  selected={false}
-                  onPress={handleFilterClick}
-                />
-              );
-            }
-          })}
-        </View>
+        <Text style={[styles.smallText, {marginLeft: 120, marginTop: 5}]}>{passchk}</Text>
         <TouchableOpacity onPress={handleClick} style={styles.registerBtn}>
           <Text style={{fontSize: 25, color: '#000'}}>가입하기</Text>
         </TouchableOpacity>
